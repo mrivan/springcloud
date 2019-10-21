9a2d736597fe383033ebf1d490dd3eacf9a8b7af
Ivan Verzilli <I.Verzilli@almaviva.it>
2019-10-21 12:33:38 +0200

    primo commit

---
+++ exampleSpringCloudServer/src/test/java/it/almaviva/springserver/ExampleSpringCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.almaviva.springserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
